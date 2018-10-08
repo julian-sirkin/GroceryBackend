139a44d7cec7397e571e8db19bcdbf21a4f3f42e
juliansirkin <juliansirkin@gmail.com>
2018-10-08 10:27:05 -0400

    Refactor
    -Url pass in search params as variable
    -Move variables from global scope to within function

--- app/routes/edaman_routes.js
+++ app/routes/edaman_routes.js
@@ -6,11 +6,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const router = express.Router()
-// Keys for edaman server
-const edamanId = process.env.EDAMAN_APPLICATION_ID
-const edamanKey = process.env.EDAMAN_APPLICATION_KEY
-// Edaman url
-const edamanUrl = `https://api.edamam.com/search?&app_id=${edamanId}&app_key=${edamanKey}&q=chicken+parm`
+
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,12 +15,23 @@ app.use(bodyParser.json())
 
 
 router.get('/edaman', (req, res) => {
-  console.log(req.body)
+  // Keys for edaman server
+  const edamanId = process.env.EDAMAN_APPLICATION_ID
+  const edamanKey = process.env.EDAMAN_APPLICATION_KEY
+
+  // Get the serach parameter from the json sent
+  const searchParam = encodeURIComponent(req.body.recipe.search)
+
+  // Edaman url
+  const edamanUrl = `https://api.edamam.com/search?&app_id=${edamanId}&app_key=${edamanKey}&q=${searchParam}`
+
+  // Call to
   axios.get(edamanUrl)
     .then(function (response) {
       const recipes = response.data.hits
       return res.status(200).json({ body: recipes })
     })
+  //return res.status(200).json({ body: 'recipes' })
 })
 
 module.exports = router
