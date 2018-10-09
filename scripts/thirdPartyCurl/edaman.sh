7161e7544c4aea4e64c9dbd46691700224e52302
juliansirkin <juliansirkin@gmail.com>
2018-10-09 16:46:02 -0400

    Change Location
    db.js
      -Update database getting written in
    
    edaman.sh
      -Change where curl script was calling as testing

--- scripts/thirdPartyCurl/edaman.sh
+++ scripts/thirdPartyCurl/edaman.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-API="https://api.edamam.com/search?&app_id=8a2e6840&app_key=0dd73c7a741de20d66c37eeda7f678cc&r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9b5945e03f05acbf9d69625138385408"
+API="https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_9b5945e03f05acbf9d69625138385408&app_id=8a2e6840&app_key=0dd73c7a741de20d66c37eeda7f678cc"
 
 curl "${API}${URL_PATH}" \
   --include \
