3a65aad501506e798aa0a70e637d969ff44fb173
juliansirkin <juliansirkin@gmail.com>
2018-10-11 15:10:40 -0400

    Add middleware to fix headers
    app.use function

--- app/routes/edaman_routes.js
+++ app/routes/edaman_routes.js
@@ -21,13 +21,30 @@ router.get('/edaman', (req, res) => {
   // Edaman url
   const edamanUrl = `https://api.edamam.com/search?&app_id=${edamanId}&app_key=${edamanKey}&q=${searchParam}`
 
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://julian-sirkin.github.io')
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    // Pass to next layer of middleware
+    next()
+})
+
+
   // Call to
   axios.get(edamanUrl)
     .then(function (response) {
       const recipes = response.data.hits
       return res.status(200).json({ body: recipes })
     })
-    .catch('fail')
+    .catch(() => {
+      res.status(400).json({'message': 'failed'})
+    })
   // return res.status(200).json({ body: 'recipes' })
 })
 
